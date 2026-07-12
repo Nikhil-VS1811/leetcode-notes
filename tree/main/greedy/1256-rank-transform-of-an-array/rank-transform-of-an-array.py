0ba096cbef4515d9e85f0a179a1721621f104768
Nikhil VS <167699325+Nikhil-VS1811@users.noreply.github.com>
2026-07-12 22:42:40 +0530

    Time: 41 ms (52.70%) | Memory: 33.6 MB (98.35%) - LeetSync

---
+++ tree/main/greedy/1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -0,0 +1,4 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        unique_sorted = sorted(set(arr))
+        return [bisect_right(unique_sorted, x) for x in arr]
